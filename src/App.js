f20ac50b2bfb41b6d162cf185960b1e9ca917333
danieldaug <danieldaug3@gmail.com>
2024-10-03 21:51:40 -0700

    changed asset locations

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import Banner from './components/Banner.tsx';
 import Intro from './components/Intro.tsx';
 import ProjectDisplay from './components/ProjectDisplay.tsx';
 import { FaArrowDown } from 'react-icons/fa';
+import DDIcon from './assets/DDIcon.png';
+import BlamLogo from './assets/CombinedLogo.png';
+import AboutImg from './assets/about.JPG';
+import PerceptronImg from './assets/perceptroninput.png';
+import PokerbotImg from './assets/pokerbot.png';
+import ProfileImg from './assets/profile.png';
+import StoneGridImg from './assets/stonegrid.jpg';
+import VideogameImg from './assets/videogame.png';
+import WordleImg from './assets/wordleimg.png';
 
 // Projects
 const projects = [
-  { title: 'Daily Disc', description: '3D mobile game where the user throws a disc and tries to score in 4 nets in as few throws as possible. New island/level every day through random perlin noise generation. Releasing a beta testing version by the end of the year and releasing the game within the following few months. Contains 100% original assets and coding with Godot.', imageUrl: '/DDIcon.png', overlayText: 'This is the logo of the Daily Disc app. Expect to see it soon in the App store!', git: 'https://github.com/tommycbird/DailyDisc' },
-  { title: 'Texas Hold\'em Bot', description: 'Program that generates thousands of data files from simulated Texas Hold\'em games and saves the data into 4 csv files. Each csv file is then transferred to a Pandas dataframe and fitted to their 4 respective perceptron machines that each predict a specific stage of Texas Hold\'em (preflop, flop, turn, river).', imageUrl: '/pokerbot.png', overlayText: 'The Poker Bot recognizes that a pair of jacks is a desirable hand without knowledge of other current hands. As a result, the bot tells the user to continue.', git: 'https://github.com/danieldaug/Texas-Holdem-Bot'},
-  { title: 'Most Stones Within Manhattan Distance', description: 'Solution to finding the location on a grid with the most stones placed within a specified distance. Solution reads in a grid containing stone locations and the maximum Manhattan distance to measure stones from. This solution utilizes dynamic programming to build a memoization grid in order to efficiently calculate the amount of stones in a given distance.', imageUrl: '/stonegrid.jpg', overlayText: 'An example of the program\'s input of stone locations on a grid', git: 'https://github.com/danieldaug/StonesWithinManhattanDistance'},
-  { title: '2D Videogame Demo', description: 'Basic 2D game including original animated sprites that collide, interact with each other, and take in user input. Built with the Godot Game Engine, coded with GDscript (similar to Python), and tested with Godot Unit Test extension.', imageUrl: '/videogame.png', overlayText: 'The player\'s character (knight) about to battle with npc (alien)', git: 'https://github.com/danieldaug/VideogameDemo' },
-  { title: 'Wordle With Tkinter', description: 'Game replicating the popular game, Wordle, with Python and the Python GUI, Tkinter. Contains different settings for games and keyboard.', imageUrl: '/wordleimg.png', overlayText: 'Wordle user interface', git: 'https://github.com/danieldaug/Wordle' },
-  { title: 'Single Layer Perceptron', description: 'Single layer perceptron that takes in matrix input and outputs a vector classifying that matrix based on previous training of weights. Comes with example files showing matrices representing letters that the perceptron can then recognize during testing.', imageUrl: '/perceptroninput.png', overlayText: 'Examples of letter inputs in the form of matrices that the perceptron will memorize.', git: 'https://github.com/danieldaug/Perceptron' },
-  { title: 'Art Website', description: 'Simple art website for a my personal art brand containing links to socials, an art gallery with a randomly generated layout of art pieces, and a setup for a future online store.', imageUrl: '/CombinedLogo.png', overlayText: '', git: 'https://github.com/danieldaug/BLAM-Website', other: 'https://blamcreative.com'},
+  { title: 'Daily Disc', description: '3D mobile game where the user throws a disc and tries to score in 4 nets in as few throws as possible. New island/level every day through random perlin noise generation. Releasing a beta testing version by the end of the year and releasing the game within the following few months. Contains 100% original assets and coding with Godot.', imageUrl: DDIcon, overlayText: 'This is the logo of the Daily Disc app. Expect to see it soon in the App store!', git: 'https://github.com/tommycbird/DailyDisc' },
+  { title: 'Texas Hold\'em Bot', description: 'Program that generates thousands of data files from simulated Texas Hold\'em games and saves the data into 4 csv files. Each csv file is then transferred to a Pandas dataframe and fitted to their 4 respective perceptron machines that each predict a specific stage of Texas Hold\'em (preflop, flop, turn, river).', imageUrl: PokerbotImg, overlayText: 'The Poker Bot recognizes that a pair of jacks is a desirable hand without knowledge of other current hands. As a result, the bot tells the user to continue.', git: 'https://github.com/danieldaug/Texas-Holdem-Bot'},
+  { title: 'Most Stones Within Manhattan Distance', description: 'Solution to finding the location on a grid with the most stones placed within a specified distance. Solution reads in a grid containing stone locations and the maximum Manhattan distance to measure stones from. This solution utilizes dynamic programming to build a memoization grid in order to efficiently calculate the amount of stones in a given distance.', imageUrl: StoneGridImg, overlayText: 'An example of the program\'s input of stone locations on a grid', git: 'https://github.com/danieldaug/StonesWithinManhattanDistance'},
+  { title: '2D Videogame Demo', description: 'Basic 2D game including original animated sprites that collide, interact with each other, and take in user input. Built with the Godot Game Engine, coded with GDscript (similar to Python), and tested with Godot Unit Test extension.', imageUrl: VideogameImg, overlayText: 'The player\'s character (knight) about to battle with npc (alien)', git: 'https://github.com/danieldaug/VideogameDemo' },
+  { title: 'Wordle With Tkinter', description: 'Game replicating the popular game, Wordle, with Python and the Python GUI, Tkinter. Contains different settings for games and keyboard.', imageUrl: WordleImg, overlayText: 'Wordle user interface', git: 'https://github.com/danieldaug/Wordle' },
+  { title: 'Single Layer Perceptron', description: 'Single layer perceptron that takes in matrix input and outputs a vector classifying that matrix based on previous training of weights. Comes with example files showing matrices representing letters that the perceptron can then recognize during testing.', imageUrl: PerceptronImg, overlayText: 'Examples of letter inputs in the form of matrices that the perceptron will memorize.', git: 'https://github.com/danieldaug/Perceptron' },
+  { title: 'Art Website', description: 'Simple art website for a my personal art brand containing links to socials, an art gallery with a randomly generated layout of art pieces, and a setup for a future online store.', imageUrl: BlamLogo, overlayText: '', git: 'https://github.com/danieldaug/BLAM-Website', other: 'https://blamcreative.com'},
 ];
 
 // Functional Component for Footer
@@ -39,7 +48,7 @@ function App() {
           <Intro
             introTitle="Hello! I'm Daniel Daugbjerg."
             introSub="Welcome to my Computer Science Portfolio."
-            profileImageSrc="/profile.png" // Update with actual image path
+            profileImageSrc={ ProfileImg }
           />
         </div>
         <div id="about-section">
@@ -76,7 +85,7 @@ function App() {
               </button>
             </div>
             <div className="about-image">
-              <img src={"/about.JPG"} alt="About" />
+              <img src={AboutImg} alt="About" />
             </div>
           </div>
         </div>
